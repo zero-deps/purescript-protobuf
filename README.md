5067a32f0b32b09ba5f444fb0e4414bd4b6e76ab
Andrii Nemchenko <a.nemchenko@icloud.com>
2019-07-23 08:31:57 +0300

    decode, encode, ops, build

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # purescript-proto
+
 Protobuf encoding/decoding for PureScript
+
+## Build
+
+```bash
+npm i
+npm run dep
+npm run com
+npm run repl
+```
+
---
+++ src/Utf8.purs
@@ -0,0 +1,8 @@
+module Proto.Utf8 where
+
+import Data.ArrayBuffer.Types (Uint8Array)
+
+foreign import numOfBytes :: String -> Int
+foreign import toString :: Uint8Array -> String
+foreign import toUint8Array :: String -> Int -> Uint8Array
+
