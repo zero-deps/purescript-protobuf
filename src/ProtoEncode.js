c8a0d6ccf5e01a1f10aeb92a38ea90a81dc9571b
Andrii Nemchenko <a.nemchenko@icloud.com>
2019-07-24 18:25:41 +0300

    inline constants

--- src/ProtoEncode.js
+++ src/ProtoEncode.js
@@ -1,5 +1,11 @@
 "use strict"
 
+var FLOAT64_MAX = 1.7976931348623157e+308
+var FLOAT64_MIN = 2.2250738585072014e-308
+var TWO_TO_20 = 1048576
+var TWO_TO_32 = 4294967296
+var TWO_TO_52 = 4503599627370496
+
 exports.splitFloat64 = function(value) {
   var sign = (value < 0) ? 1 : 0
   value = sign ? -value : value
@@ -21,15 +27,15 @@ exports.splitFloat64 = function(value) {
   }
 
   // Handle infinities.
-  if (value > jspb.BinaryConstants.FLOAT64_MAX) {
+  if (value > FLOAT64_MAX) {
     return { low: 0, high: ((sign << 31) | (0x7FF00000)) >>> 0 }
   }
 
   // Handle denormals.
-  if (value < jspb.BinaryConstants.FLOAT64_MIN) {
+  if (value < FLOAT64_MIN) {
     // Number is a denormal.
     var mant = value / Math.pow(2, -1074)
-    var mantHigh = (mant / jspb.BinaryConstants.TWO_TO_32)
+    var mantHigh = (mant / TWO_TO_32)
     return { low: (mant >>> 0), high: ((sign << 31) | mantHigh) >>> 0 }
   }
 
@@ -37,8 +43,8 @@ exports.splitFloat64 = function(value) {
   if (exp == 1024) exp = 1023
   var mant = value * Math.pow(2, -exp)
 
-  var mantHigh = (mant * jspb.BinaryConstants.TWO_TO_20) & 0xFFFFF
-  var mantLow = (mant * jspb.BinaryConstants.TWO_TO_52) >>> 0
+  var mantHigh = (mant * TWO_TO_20) & 0xFFFFF
+  var mantLow = (mant * TWO_TO_52) >>> 0
 
   return { low: mantLow, high: ((sign << 31) | ((exp + 1023) << 20) | mantHigh) >>> 0 }
 }
