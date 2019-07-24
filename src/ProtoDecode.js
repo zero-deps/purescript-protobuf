bb98d687a222540eb493bafc7ab99fd1ff3ed371
Andrii Nemchenko <a.nemchenko@icloud.com>
2019-07-24 18:21:51 +0300

    inline constants

--- src/ProtoDecode.js
+++ src/ProtoDecode.js
@@ -1,10 +1,13 @@
 "use strict"
 
+var TWO_TO_32 = 4294967296
+var TWO_TO_52 = 4503599627370496
+
 exports.joinFloat64 = function(bitsLow) {
   return function(bitsHigh) {
     var sign = ((bitsHigh >> 31) * 2 + 1)
     var exp = (bitsHigh >>> 20) & 0x7FF
-    var mant = jspb.BinaryConstants.TWO_TO_32 * (bitsHigh & 0xFFFFF) + bitsLow
+    var mant = TWO_TO_32 * (bitsHigh & 0xFFFFF) + bitsLow
 
     if (exp == 0x7FF) {
       if (mant) {
@@ -19,8 +22,7 @@ exports.joinFloat64 = function(bitsLow) {
       return sign * Math.pow(2, -1074) * mant
     } else {
       return sign * Math.pow(2, exp - 1075) *
-             (mant + jspb.BinaryConstants.TWO_TO_52)
+             (mant + TWO_TO_52)
     }
   }
 }
-
